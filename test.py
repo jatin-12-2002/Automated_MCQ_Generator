293ee0e87b2f73e8c04ec8cd51147a507cdc142c
JATIN SAREEN <102468240+jatin-12-2002@users.noreply.github.com>
2024-08-11 18:47:49 +0000

    testing done

--- test.py
+++ test.py
@@ -4,13 +4,13 @@ import json
 import traceback
 import pandas as pd
 from dotenv import load_dotenv
-from mcqgenrator.utils import read_file,get_table_data
+from MCQGenerator.utils import read_file,get_table_data
 import streamlit as st
 from langchain.callbacks import get_openai_callback
-from mcqgenrator.MCQGenrator import generate_evaluate_chain
+from MCQGenerator.MCQGenerator import generate_evaluate_chain
 
 #loading json file
-with open('C:\Complete_Content\All_Project\TEST_FOR_EVERYTHING\langchain\Response.json', 'r') as file:
+with open('/workspaces/Automated_MCQ_Generator/Response.json', 'r') as file:
     RESPONSE_JSON = json.load(file)
 
 #print(RESPONSE_JSON)
